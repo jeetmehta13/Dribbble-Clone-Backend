b482c9ec9543b0714d0a5026ef2b299fc6868ab7
Shush <shushrut.ss.sharma@gmail.com>
2020-10-01 23:09:09 +0530

    list.js updated

--- models/List.js
+++ models/List.js
@@ -18,8 +18,8 @@ export default class List {
 
     deleteItem(id) {
         const index = this.items.findIndex(el => el.id === id);
-        [2, 4, 8] splice(1, 2) - > returns[4, 8], original array is[2]
-            [2, 4, 8] slice(1, 2) - > returns 4, original array is[2, 4, 8]
+        // [2, 4, 8] splice(1, 2) - > returns[4, 8], original array is[2]
+        //     [2, 4, 8] slice(1, 2) - > returns 4, original array is[2, 4, 8]
         this.items.splice(index, 1);
     }
 
